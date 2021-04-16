17a0407e2beccfb0141a42db7ad92c04802ef75d
Nikolaenkov Roman <rnikolaenkov@yandex.ru>
2021-04-16 20:37:02 +0000

    added category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,15 +6,11 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
         $this->call([
-            ProductSeeder::class
+            ProductSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
