4e5f964e1bdefa167ea973da10a666579aea5a91
Nikolaenkov Roman <rnikolaenkov@yandex.ru>
2021-04-16 07:52:12 +0000

    added main configuration, and added default limit and offset

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -9,15 +9,33 @@ use App\Models\Product;
 class ProductService
 {
     protected $mProduct;
+    protected $limit;
+    protected $offset;
 
     public function __construct(Product $product)
     {
         $this->mProduct = $product;
+        $this->limit = config('main.limit');
+        $this->offset = config('main.offset');
     }
 
     public function getProducts(array $data)
     {
-        $products = $this->mProduct->get();
+        $request = $this->mProduct;
+
+        if (isset($data['offset'])) {
+            $request = $request->offset($data['offset']);
+        } else {
+            $request = $request->offset($this->offset);
+        }
+
+        if (isset($data['limit'])) {
+            $request = $request->limit($data['limit']);
+        } else {
+            $request = $request->limit($this->limit);
+        }
+
+        $products = $request->get();
 
         return $products;
     }
