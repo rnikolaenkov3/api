b766f2f8f94d05a35c91db27184d7d01a13eba0b
Nikolaenkov Roman <rnikolaenkov@yandex.ru>
2021-04-16 07:44:02 +0000

    get all products

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -4,17 +4,25 @@
 namespace App\Http\Controllers\Api\V1;
 
 
-use Illuminate\Support\Facades\Request;
+use App\Services\ProductService;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function __construct()
-    {
+    protected $sProduct;
 
+    public function __construct(ProductService $productService)
+    {
+        $this->sProduct = $productService;
     }
 
     public function getProducts(Request $request)
     {
+        $data = $request->all();
+
+        $products = $this->sProduct->getProducts($data);
+
+        dd($products);
         return response()->json(['status' => 'ok']);
     }
 }
